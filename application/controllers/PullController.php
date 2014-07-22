9a3122b594c51c3e7899b61830c58d31cd046692
rcastley <rcastley@rcastley.home>
2014-07-22 19:47:23 +0100

    Support for any Pull API request

--- application/controllers/PullController.php
+++ application/controllers/PullController.php
@@ -1,8 +1,10 @@
 <?php
 class PullController extends Zend_Controller_Action {
 	protected $_user;
-	protected $_key = 'RY-Pw-jRXRWW9PsE';
-	protected $_secret = '16409a47-ac89-42f1-811c-0fe666ab4138';
+	protected $_tokenUrl = 'https://ioqa.catchpoint.com/HawkUI/api/';
+	protected $_url = 'https://ioqa.catchpoint.com/HawkUI/api/v1/';
+	protected $_key = 'F0-C-jQ6Ni_.BAi1';
+	protected $_secret = 'ded28672-81f5-4877-884c-ad1ffc7e8530';
 	protected $_token;
 	protected $_expires;
 	public $session;
@@ -15,7 +17,7 @@ class PullController extends Zend_Controller_Action {
 	}
 	public function init() {
 		$this->session = new Zend_Session_Namespace ( 'Catchpoint' );
-		
+
 		$this->_user = Zend_Auth::getInstance ()->getIdentity ();
 		$this->view->user = $this->_user;
 		
@@ -24,7 +26,7 @@ class PullController extends Zend_Controller_Action {
 		$site = new Application_Model_SiteMapper ();
 		
 		$result = $site->fetchAll ( $this->_user );
-		
+
 		if ($result->key == null) {
 			$this->view->settings = 'please enter your key';
 		} else {
@@ -44,11 +46,11 @@ class PullController extends Zend_Controller_Action {
 	}
 	private function getToken() {
 		$ch = curl_init ();
-		
-		curl_setopt ( $ch, CURLOPT_URL, 'https://io.catchpoint.com/ui/api/token' );
+		echo $this->_url . 'token';
+		curl_setopt ( $ch, CURLOPT_URL, $this->_tokenUrl . 'token' );
 		curl_setopt ( $ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials&client_id=' . $this->_key . '&client_secret=' . $this->_secret );
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt ( $ch, CURLOPT_VERBOSE, true );
+		//curl_setopt ( $ch, CURLOPT_VERBOSE, true );
 		
 		$result = curl_exec ( $ch );
 		
@@ -59,18 +61,22 @@ class PullController extends Zend_Controller_Action {
 		$this->_token = $jsonResponse->access_token;
 		$this->_expires = $jsonResponse->expires_in;
 	}
-	public function nodesAction() {
+	public function apiAction() {
+		$request = $this->_getParam('call');
 		$ch = curl_init ();
 		
-		curl_setopt ( $ch, CURLOPT_URL, 'https://io.catchpoint.com/ui/api/v1/nodes' );
+		curl_setopt ( $ch, CURLOPT_URL, $this->_url . $request);
 		curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
 				'Authorization: Bearer ' . $this->session->token 
 		) );
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-		$nodes = curl_exec ( $ch ) ;
-		
+		$nodes = json_decode(curl_exec ( $ch ) );
 		curl_close ( $ch );
-		$this->view->nodes = $nodes;
+		echo '<pre>';
+		print_r ($nodes);
+		//foreach ($nodes->items as $k => $v) {
+		//	$this->view->name = array($v);
+		//}
 	}
 
 }
