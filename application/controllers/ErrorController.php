2dc5942c42f53ab8be75723a2c929071959f1166
rcastley <robert.castley@gmail.com>
2015-10-17 00:33:59 +0100

    Formatting

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -2,18 +2,17 @@
 
 class ErrorController extends Zend_Controller_Action
 {
-
-    public function errorAction ()
+    public function errorAction()
     {
-        $this->_helper->layout->setLayout('basic');
-        
+        //$this->_helper->layout->setLayout('basic');
+
         $errors = $this->_getParam('error_handler');
-        
+
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-                
+
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
                 $this->view->message = 'Page not found';
@@ -24,28 +23,28 @@ class ErrorController extends Zend_Controller_Action
                 $this->view->message = 'Application error';
                 break;
         }
-        
+
         // Log exception, if logger available
         if ($log = $this->getLog()) {
             $log->crit($this->view->message, $errors->exception);
         }
-        
+
         // conditionally display exceptions
         if ($this->getInvokeArg('displayExceptions') == true) {
             $this->view->exception = $errors->exception;
         }
-        
+
         $this->view->request = $errors->request;
     }
 
-    public function getLog ()
+    public function getLog()
     {
         $bootstrap = $this->getInvokeArg('bootstrap');
-        if (! $bootstrap->hasPluginResource('Log')) {
+        if (!$bootstrap->hasPluginResource('Log')) {
             return false;
         }
         $log = $bootstrap->getResource('Log');
+
         return $log;
     }
 }
-
