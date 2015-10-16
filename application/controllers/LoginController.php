69c98bc66f462aefc26dbafe4166df61b757ad2c
rcastley <robert.castley@gmail.com>
2015-10-17 00:34:45 +0100

    Moved to Catchpoint

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -2,22 +2,21 @@
 
 class LoginController extends Zend_Controller_Action
 {
-
-    public function init ()
+    public function init()
     {
         $this->_helper->layout->setLayout('basic');
     }
 
-    public function getForm ()
+    public function getForm()
     {
         return new Application_Form_Login(
                 array(
                         'action' => '/login/process',
-                        'method' => 'post'
+                        'method' => 'post',
                 ));
     }
 
-    public function preDispatch ()
+    public function preDispatch()
     {
         if (Zend_Auth::getInstance()->hasIdentity()) {
             // If the user is logged in, we don't want to show the login form;
@@ -34,43 +33,45 @@ class LoginController extends Zend_Controller_Action
         }
     }
 
-    public function indexAction ()
+    public function indexAction()
     {
         $this->view->form = $this->getForm();
     }
 
-    public function processAction ()
+    public function processAction()
     {
         $request = $this->getRequest();
-        
+
         // Check if we have a POST request
-        if (! $request->isPost()) {
+        if (!$request->isPost()) {
             return $this->_helper->redirector('index');
         }
-        
+
         // Get our form and validate it
         $form = $this->getForm();
-        if (! $form->isValid($request->getPost())) {
+        if (!$form->isValid($request->getPost())) {
             // Invalid entries
             $this->view->form = $form;
+
             return $this->render('index'); // re-render the login form
         }
-        
+
         $auth = Zend_Auth::getInstance();
-        $result = $auth->authenticate(new My_Auth_Adapter($form->getValues()));
-        
-        if (! $result->isValid()) {
+        $result = $auth->authenticate(new Catchpoint_Auth_Adapter($form->getValues()));
+
+        if (!$result->isValid()) {
             // Invalid credentials
             $form->setDescription('Invalid credentials provided');
             $this->view->form = $form;
+
             return $this->render('index'); // re-render the login form
         }
-        
+
         // We're authenticated! Redirect to the home page
         $this->_helper->redirector('index', 'index');
     }
 
-    public function logoutAction ()
+    public function logoutAction()
     {
         Zend_Auth::getInstance()->clearIdentity();
         Zend_Session::namespaceUnset('Catchpoint');
