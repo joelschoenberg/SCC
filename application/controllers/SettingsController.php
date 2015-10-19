871b40eb7f7a3267051d6abd045f0f36bbb1d3ac
rcastley <robert.castley@gmail.com>
2015-10-19 08:23:07 +0100

    Added chart_id

--- application/controllers/SettingsController.php
+++ application/controllers/SettingsController.php
@@ -2,46 +2,44 @@
 
 class SettingsController extends Zend_Controller_Action
 {
-
     protected $_user;
 
-    public function preDispatch ()
+    public function preDispatch()
     {
-        if (! Zend_Auth::getInstance()->hasIdentity()) {
+        if (!Zend_Auth::getInstance()->hasIdentity()) {
             // If the user is logged in, we don't want to show the login form;
             // however, the logout action should still be available
             $this->_helper->redirector('index', 'login');
         }
     }
 
-    public function init ()
+    public function init()
     {
         $this->_user = Zend_Auth::getInstance()->getIdentity();
         $this->view->user = $this->_user;
-        
+
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
     }
 
-    public function indexAction ()
-    {}
+    public function indexAction()
+    {
+    }
 
-    public function saveAction ()
+    public function saveAction()
     {
         $settings = new Application_Model_Site(
                 array(
                         'user' => $this->_user,
                         'key' => $this->_getParam('key'),
-                        'secret' => $this->_getParam('secret')
+                        'secret' => $this->_getParam('secret'),
+                        'chartId' => $this->_getParam('chart_id')
                 ));
-        
+
         $mapper = new Application_Model_SiteMapper();
-        
+
         $mapper->settings($settings);
-        
+
         $this->_helper->redirector('index', 'index');
     }
 }
-
-
-
