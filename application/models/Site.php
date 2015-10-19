14b44b1df4c84ad68bd79735984db76532a9cd67
rcastley <robert.castley@gmail.com>
2015-10-19 08:22:26 +0100

    Added chart_id

--- application/models/Site.php
+++ application/models/Site.php
@@ -2,7 +2,6 @@
 
 class Application_Model_Site
 {
-
     protected $_id;
 
     protected $_user;
@@ -13,94 +12,115 @@ class Application_Model_Site
 
     protected $_secret;
 
-    public function __construct (array $options = null)
+    protected $_chart_id;
+
+    public function __construct(array $options = null)
     {
         if (is_array($options)) {
             $this->setOptions($options);
         }
     }
 
-    public function __set ($name, $value)
+    public function __set($name, $value)
     {
-        $method = 'set' . $name;
-        if (('mapper' == $name) || ! method_exists($this, $method)) {
+        $method = 'set'.$name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
             throw new Exception('Invalid property');
         }
         $this->$method($value);
     }
 
-    public function __get ($name)
+    public function __get($name)
     {
-        $method = 'get' . $name;
-        if (('mapper' == $name) || ! method_exists($this, $method)) {
+        $method = 'get'.$name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
             throw new Exception('Invalid property');
         }
+
         return $this->$method();
     }
 
-    public function setOptions (array $options)
+    public function setOptions(array $options)
     {
         $methods = get_class_methods($this);
         foreach ($options as $key => $value) {
-            $method = 'set' . ucfirst($key);
+            $method = 'set'.ucfirst($key);
             if (in_array($method, $methods)) {
                 $this->$method($value);
             }
         }
+
         return $this;
     }
 
-    public function setUser ($text)
+    public function setUser($text)
     {
         $this->_user = (string) $text;
+
         return $this;
     }
 
-    public function getUser ()
+    public function getUser()
     {
         return $this->_user;
     }
 
-    public function setState ($ts)
+    public function setState($ts)
     {
         $this->_state = $ts;
+
         return $this;
     }
 
-    public function getState ()
+    public function getState()
     {
         return $this->_state;
     }
 
-    public function setId ($id)
+    public function setId($id)
     {
         $this->_id = (int) $id;
+
         return $this;
     }
 
-    public function getId ()
+    public function getId()
     {
         return $this->_id;
     }
 
-    public function setKey ($ts)
+    public function setChartId($chart_id)
     {
-        $this->_key = $ts;
+        $this->_chart_id = (int) $chart_id;
+
         return $this;
     }
 
-    public function getKey ()
+    public function getChartId()
+    {
+        return $this->_chart_id;
+    }
+
+    public function setKey($ts)
+    {
+        $this->_key = $ts;
+
+        return $this;
+    }
+
+    public function getKey()
     {
         return $this->_key;
     }
 
-    public function setSecret ($ts)
+    public function setSecret($ts)
     {
         $this->_secret = $ts;
+
         return $this;
     }
 
-    public function getSecret ()
+    public function getSecret()
     {
         return $this->_secret;
     }
--- application/models/SiteMapper.php
+++ application/models/SiteMapper.php
@@ -2,73 +2,76 @@
 
 class Application_Model_SiteMapper
 {
-
     protected $_dbTable;
 
-    public function setDbTable ($dbTable)
+    public function setDbTable($dbTable)
     {
         if (is_string($dbTable)) {
             $dbTable = new $dbTable();
         }
-        if (! $dbTable instanceof Zend_Db_Table_Abstract) {
+        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
             throw new Exception('Invalid table data gateway provided');
         }
         $this->_dbTable = $dbTable;
+
         return $this;
     }
 
-    public function getDbTable ()
+    public function getDbTable()
     {
         if (null === $this->_dbTable) {
             $this->setDbTable('Application_Model_DbTable_Site');
         }
+
         return $this->_dbTable;
     }
 
-    public function create (Application_Model_Site $site)
+    public function create(Application_Model_Site $site)
     {
         $data = array(
                 'user' => $site->getUser(),
-                'state' => $site->getState()
+                'state' => $site->getState(),
         );
-        
+
         $this->getDbTable()->insert($data);
     }
 
-    public function save (Application_Model_Site $site)
+    public function save(Application_Model_Site $site)
     {
         $data = array(
                 'user' => $site->getUser(),
-                'state' => $site->getState()
+                'state' => $site->getState(),
         );
-        
-        $this->getDbTable()->update($data, 
+
+        $this->getDbTable()->update($data,
                 array(
-                        'user = ?' => $site->getUser()
+                        'user = ?' => $site->getUser(),
                 ));
     }
 
-    public function settings (Application_Model_Site $site)
+    public function settings(Application_Model_Site $site)
     {
         $data = array(
                 'user' => $site->getUser(),
                 'key' => $site->getKey(),
-                'secret' => $site->getSecret()
+                'secret' => $site->getSecret(),
+                'chart_id' => $site->getChartId(),
         );
-        
-        $this->getDbTable()->update($data, 
+
+        print_r($data);
+        $this->getDbTable()->update($data,
                 array(
-                        'user = ?' => $site->getUser()
+                        'user = ?' => $site->getUser(),
                 ));
     }
 
-    public function fetchAll ($user)
+    public function fetchAll($user)
     {
         $query = $this->getDbTable()
             ->select()
             ->where('user = ?', $user);
         $resultSet = $this->getDbTable()->fetchRow($query);
+
         return $resultSet;
     }
 }
-
