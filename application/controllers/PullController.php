72f9525aab57f9dbfa6c531925dd6e875f605d12
rcastley <robert.castley@gmail.com>
2015-10-21 10:48:25 +0100

    Controller uses Library pull

--- application/controllers/PullController.php
+++ application/controllers/PullController.php
@@ -4,17 +4,9 @@ class PullController extends Zend_Controller_Action
 {
     protected $_user;
 
-    protected $_tokenUrl = 'https://io.catchpoint.com/ui/api/';
-
-    protected $_url = 'https://io.catchpoint.com/ui/api/v1/';
-
     protected $_key;
 
     protected $_secret;
-
-    protected $_token;
-
-    protected $_expires;
 
     protected $_session;
 
@@ -42,12 +34,8 @@ class PullController extends Zend_Controller_Action
         if ($result->key == null) {
             throw new Exception('No Key/Secret set!');
         } else {
-            $this->_key = base64_decode($result->key);
-            $this->_secret = base64_decode($result->secret);
-
-            if (!isset($this->_session->token) || $this->_session->token == '') {
-                $this->getToken();
-            }
+            $this->_key = $result->key;
+            $this->_secret = $result->secret;
         }
     }
 
@@ -55,59 +43,19 @@ class PullController extends Zend_Controller_Action
     {
     }
 
-    private function getToken()
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->_tokenUrl.'token');
-        curl_setopt($ch, CURLOPT_POSTFIELDS,
-                'grant_type=client_credentials&client_id='.$this->_key.
-                '&client_secret='.$this->_secret);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        /*
-        curl_setopt($ch, CURLOPT_STDERR, fopen('php://output', 'w+'));
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        */
-
-        $result = curl_exec($ch);
-
-        curl_close($ch);
-
-        $jsonResponse = json_decode($result);
-
-        if ($jsonResponse->Message) {
-            throw new Exception($jsonResponse->Message);
-        } else {
-            $this->_token = $jsonResponse->access_token;
-            $this->_expires = $jsonResponse->expires_in;
-            $this->_session->token = base64_encode($this->_token);
-            $this->_session->setExpirationSeconds($this->_expires);
-        }
-    }
-
     public function apiAction()
     {
-        echo '<pre>';
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
         $request = $this->_getParam('call');
-        $ch = curl_init();
 
-        echo $this->_url.$request."\r\n";
-        curl_setopt($ch, CURLOPT_URL, $this->_url.$request);
-        curl_setopt($ch, CURLOPT_HTTPHEADER,
-                array(
-                        'Authorization: Bearer '.$this->_session->token,
-                ));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_STDERR, fopen('php://output', 'w+'));
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        $t = new Catchpoint_Pull();
+        $t->override = true;
+        $t->key = $this->_key;
+        $t->secret = $this->_secret;
 
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        if (isset($result->Message)) {
-          throw new Exception($result->Message);
-        }
-
-        print_r(json_decode($result));
-        echo '</pre>';
+        $result = $t->fetchData($request);
+        print_r ($result);
     }
 }
